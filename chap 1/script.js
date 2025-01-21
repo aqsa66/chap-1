aece72f7859ffd31741b98ebdd46271df3f63c33
aqsa <123245279+aqsa66@users.noreply.github.com>
2025-01-21 12:47:32 +0500

    Add files via upload

---
+++ chap 1/script.js
@@ -0,0 +1,64 @@
+// Question 1: Declare a variable called username
+var username;
+
+// Question 2: Declare a variable called myName and assign your Full Name
+var myName = "Fatima Khalid";
+
+// Question 3:
+// a) Declare a JS variable, titled message
+var message;
+
+// b) Assign “Hello World” to variable message
+message = "Hello World";
+
+// c) Display the message in an alert box
+alert(message);
+
+// Declare variables for the student's bio data
+var name = "Jhone Doe";
+var age = "15 years old";
+var course = "Certified Mobile Application Development";
+
+// Display the bio data in alert boxes
+alert(name);
+alert(age);
+alert(course);
+
+// Question 5: Display the alert with one variable
+// Declare a variable with the required string
+var pizza = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+
+// Display the string in an alert box
+alert(pizza);
+
+// Question 6: Display email message using string concatenation
+// Declare a variable for the email
+var email = "example@example.com";
+
+// Display the message in an alert box
+alert("My email address is " + email);
+
+// Question 7: Display the message in an alert box
+// Declare a variable called book and assign the given value
+var book = "A smarter way to learn JavaScript";
+
+// Display the message in an alert box
+alert("I am trying to learn from the Book " + book);
+
+// Question 8: Display HTML content through JavaScript in the browser
+// Use JavaScript to display content in the browser
+document.write("Yah! I can write HTML content through JavaScript");
+
+// Question 9:
+// Store the string in a variable
+var decorativeString = "══════ஜ۩۞۩ஜ══════";
+
+// Show the string in an alert
+alert(decorativeString);
+
+// Display the string on the webpage
+document.write(decorativeString);
+     
+// tea_love
+// zainabz8972@gmail.com
+// zainab2798@gmail.com
